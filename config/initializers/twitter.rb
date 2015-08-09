10f44761fa555d29990d7d4b790d99cf2a00eafd
Michael Christofersen <mchristofersen@gmail.com>
2015-08-09 16:53:03 -0500

    fixes

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+$twitter = Twitter::REST::Client.new do |config|
+  config.consumer_key = lqlwsJetc3alvSxg9YBiR1xOo
+  config.consumer_secret = beLqf4DLqJ6gl2vy7Q1HQCCPcwFJaiFXDMqte2SFuXotcV3PlF
+  config.access_token = 448127871-p4BDXNfLBw8pkyzGFaUSm5ilVeQp1moEda1Zbus4
+  config.access_token_secret = mMnhrAG6iZ6O5D6oASXXQjdb1uCyCqurmEhQhCQXv1O9F
+end
